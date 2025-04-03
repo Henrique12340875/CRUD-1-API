e6489913693fa8d4678b7db076464cb42ae564f3
Henrique12340875 <henriquegames1409@gmail.com>
2025-04-03 15:23:13 -0300

    feat: connection test

--- db/connect.rb
+++ db/connect.rb
@@ -7,4 +7,12 @@ db_path = File.join(project_root,'storage','db.sqlite3')
 
 FileUtils.mkdir_p(File.dirname(db_path))
 
-@DB = Sequel.sqlite(db_path)
+begin
+    @DB = Sequel.sqlite(db_path)
+    @DB.test_connection
+    puts "Conectado ao banco de dados!"
+rescue Sequel::DatabaseConnectionError => e
+    puts "Erro:#{e.message}"
+end
+
+
